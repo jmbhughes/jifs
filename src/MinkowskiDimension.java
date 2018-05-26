8b30c52fc4a992875d91a55af9140afd71b6dc8c
jmbhughes <hughes.jmb@gmail.com>
2018-05-26 16:15:51 -0400

    stable

--- src/MinkowskiDimension.java
+++ src/MinkowskiDimension.java
@@ -1,10 +1,39 @@
+import java.util.Vector;
+/*** NOT FINISHED ***/
+
 /**
- *
+ * Calculate the box-counting/Minkowski dimension for a set of points
  */
 public class MinkowskiDimension {
-    Vector<Vector<double>> pointSet;
+    int dimension;
+    Vector<Vector<Double>> pointSet;
+    Vector<Double> sizes;
+    Vector<Integer> counts;
 
-    MinkowskiDimension(Vector<Vector<double>> pointSet) {
-        
+    /**
+     * TODO
+     */
+    MinkowskiDimension(int dimension, Vector<Vector<Double>> pointSet) {
+        this.dimension = dimension;
+        this.pointSet = pointSet;
+        for (Vector<Double> v : pointSet) {
+            if (v.size() != dimension) {
+                throw new RuntimeException("All points must be of the same dimension.");
+            }
+        }
+        this.counts = new Vector<Integer>();
+        this.sizes = new Vector<Double>();
+    }
+
+    private int count_boxes() {
+        return 0; // TODO        
+    }
+    
+
+    /**
+     * testing function
+     */
+    public static void main(String[] args) {
+        System.out.println("NOT FINISHED")
     }
 }
